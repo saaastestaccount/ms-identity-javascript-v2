afc8a0ce5c52e5fb0eed191d67b62b5f3a4138fd
saaastestaccount <149160558+saaastestaccount@users.noreply.github.com>
2023-10-27 16:04:58 +0000

    Update authConfig.js

--- app/authConfig.js
+++ app/authConfig.js
@@ -6,11 +6,11 @@
 const msalConfig = {
     auth: {
         // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
-        clientId: "Enter_the_Application_Id_Here",
+        clientId: "4acf45ed-70c5-4500-acbd-9f08c0ad8248",
         // Full directory URL, in the form of https://login.microsoftonline.com/<tenant-id>
-        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
+        authority: "https://login.microsoftonline.com/common",
         // Full redirect URL, in form of http://localhost:3000
-        redirectUri: "Enter_the_Redirect_Uri_Here",
+        redirectUri: "http://localhost:3000/index.html",
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
